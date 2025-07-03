7263f70c42ac49fa02a2054119b0531993b784ed
madhurstudies <madhur.studies24x7@gmail.com>
2025-07-03 23:32:56 +0530

    modified Armstrong.java and tested modulus function

--- D2-loopsAndMisc/Armstrong.java
+++ D2-loopsAndMisc/Armstrong.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 
 public class Armstrong {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int x = sc.nextInt();
+        
     }
 }
