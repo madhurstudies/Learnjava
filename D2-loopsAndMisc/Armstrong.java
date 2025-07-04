c386a1221caa3f27d53ef2b5a473541fd1ff5747
madhurstudies <madhur.studies24x7@gmail.com>
2025-07-04 13:46:24 +0530

    end of D2 with modif.

--- D2-loopsAndMisc/Armstrong.java
+++ D2-loopsAndMisc/Armstrong.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Armstrong {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
         
     }
 }
