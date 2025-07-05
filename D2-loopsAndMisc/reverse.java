8f759631b379ca99037bb627b7211a6a1c0f9908
madhurstudies <madhur.studies24x7@gmail.com>
2025-07-05 18:16:29 +0530

    updated reverse.java

--- D2-loopsAndMisc/reverse.java
+++ D2-loopsAndMisc/reverse.java
@@ -5,12 +5,13 @@ public class reverse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter.");
-        int x = sc.nextInt();
-        System.out.println("-----");
-        while (x >= 1) {
-            System.out.println(x);
-            x--;
-        }   
-        System.out.println("-----");
+        int num = sc.nextInt();
+        int rem = num;
+        while (num != 0) {
+            rem = num % 10;
+            num = num / 10;
+            System.out.print(rem);
+        }
+        
     }
 }
---
+++ moreLoops/fibonnaci.java
@@ -0,0 +1,5 @@
+package moreLoops;
+
+public class fibonnaci {
+    
+}
