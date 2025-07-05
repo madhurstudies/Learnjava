ca8fd91600074997416fbb3f02ad620f3e65fdf6
madhurstudies <madhur.studies24x7@gmail.com>
2025-07-05 17:48:03 +0530

    done armstrong

--- D2-loopsAndMisc/Armstrong.java
+++ D2-loopsAndMisc/Armstrong.java
@@ -3,7 +3,26 @@ import java.util.Scanner;
 public class Armstrong {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("X = ");
         int x = sc.nextInt();
-        
+        int rem = x;
+        int check = x;
+        int sum = 0;
+        int debug = 1;
+        System.out.println();
+        System.out.println("iterate wise status");
+        System.out.println("no     x    rem     sum");
+        while (x != 0) {
+            rem = x % 10;
+            x = x / 10;
+            sum = sum + rem*rem*rem;
+            System.out.println(debug+"     "+x + "     " + rem + "      " + sum);
+            debug++;
+        }
+        if (sum == check) {
+            System.out.println("Armstrong number.");
+        } else {
+            System.out.println("not armstrong.");
+        }
     }
 }
